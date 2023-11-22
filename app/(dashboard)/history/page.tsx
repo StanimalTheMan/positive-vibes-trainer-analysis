9e6efc1125b4e7784b2b8421d054c928ff004e87
Stan Choi <sjc591@nyu.edu>
2023-11-22 13:14:51 -0500

    Chart positive sentiment scores

--- app/(dashboard)/history/page.tsx
+++ app/(dashboard)/history/page.tsx
@@ -1,15 +1,37 @@
+import HistoryChart from '@/components/HistoryChart'
 import { getUserByClerkID } from '@/utils/auth'
 import { prisma } from '@/utils/db'
 
 const getData = async () => {
   const user = await getUserByClerkID()
   const analyses = await prisma.analysis.findMany({
-    where: {},
+    where: {
+      userId: user.id,
+    },
+    orderBy: {
+      createdAt: 'asc',
+    },
   })
+
+  const sum = analyses.reduce(
+    (sum, current) => sum + current.positiveSentimentScore * 100,
+    0
+  )
+  const avg = Math.round(sum / analyses.length)
+  return { analyses, avg }
 }
 
-const History = () => {
-  return <div>history</div>
+const History = async () => {
+  const { avg, analyses } = await getData()
+
+  return (
+    <div className="w-full h-full">
+      <div>{`Avg Sentiment: ${avg}% Positive Sentiment`}</div>
+      <div className="w-full h-full">
+        <HistoryChart data={analyses} />
+      </div>
+    </div>
+  )
 }
 
 export default History
