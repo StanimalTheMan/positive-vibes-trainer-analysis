c63b235f5c0bec16ff8076b44660b70f00bdcc6f
Stan Choi <sjc591@nyu.edu>
2023-11-22 12:21:35 -0500

    Create similarity searches w/ vectors and search api endpoint w/ some result

--- utils/ai.ts
+++ utils/ai.ts
@@ -6,6 +6,11 @@ import {
   DetectSentimentCommand,
 } from '@aws-sdk/client-comprehend' // ES Modules import
 import z from 'zod'
+import { Document } from 'langchain/document'
+import { loadQARefineChain } from 'langchain/chains'
+import { OpenAIEmbeddings } from 'langchain/embeddings/openai'
+import { MemoryVectorStore } from 'langchain/vectorstores/memory'
+
 // require('dotenv').config()
 
 const config = {
@@ -51,4 +56,25 @@ export const analyze = async (content) => {
   const command = new DetectSentimentCommand(input)
   const response = await client.send(command)
   return response
+}
+
+export const qa = async (question, entries) => {
+  const docs = entries.map((entry) => {
+    return new Document({
+      pageContent: entry.content,
+      metadata: { id: entry.id, createdAt: entry.createdAt },
+    })
+  })
+
+  const model = new OpenAI({ temperature: 0, modelName: 'gpt-3.5-turbo' })
+  const chain = loadQARefineChain(model)
+  const embeddings = new OpenAIEmbeddings()
+  const store = await MemoryVectorStore.fromDocuments(docs, embeddings)
+  const relevantDocs = await store.similaritySearch(question)
+  const res = await chain.call({
+    input_documents: relevantDocs,
+    question,
+  })
+
+  return res.output_text
 }
