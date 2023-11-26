152ef3fcbc0f366c4316e3462163958b54644586
Stan Choi <sjc591@nyu.edu>
2023-11-26 02:23:17 -0500

    Rid of pretty build in userButton to custom redirect via standard sign out button

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,4 +1,7 @@
-import { UserButton } from '@clerk/nextjs'
+'use client'
+
+import { useClerk } from '@clerk/clerk-react'
+import { useRouter } from 'next/navigation'
 import Link from 'next/link'
 
 const links = [
@@ -8,6 +11,9 @@ const links = [
 ]
 
 const DashboardLayout = ({ children }) => {
+  const { signOut } = useClerk()
+  const router = useRouter()
+
   return (
     <div className="h-screen w-screen relative">
       <aside className="absolute w-[200px] top-0 left-0 h-full border-r border-black/10">
@@ -23,7 +29,9 @@ const DashboardLayout = ({ children }) => {
       <div className="ml-[200px] h-full">
         <header className="h-[60px] border-b border-black/10">
           <div className="h-full w-full px-6 flex items-center justify-end">
-            <UserButton />
+            <button onClick={() => signOut(() => router.push('/journal'))}>
+              Sign out
+            </button>
           </div>
         </header>
         <div className="h-[calc(100vh-60px)]">{children}</div>
