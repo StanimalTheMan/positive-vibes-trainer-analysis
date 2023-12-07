05e0ab57fbcaa1cbd6f667c18ece8df0337ce916
Stan Choi <sjc591@nyu.edu>
2023-12-06 19:00:27 -0500

    Update home page text and button color / text color

--- app/page.tsx
+++ app/page.tsx
@@ -8,14 +8,14 @@ export default async function Home() {
   return (
     <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
       <div className="w-full max-w-[600px] mx-auto">
-        <h1 className="text-6xl mb-4">The best Journal app, period.</h1>
+        <h1 className="text-6xl mb-4">Train to journal only positive vibes.</h1>
         <p className="2-xl text-white/60 mb-4">
           This is the best app for tracking your positivity in your day to day
           life. All you have to do is be honest and try to see the positives.
         </p>
         <div>
           <Link href={href}>
-            <button className="bg-blue-600 px-4 py-2 rounded-lg text-xl">
+            <button className="bg-white text-black/60 px-4 py-2 rounded-lg text-xl">
               get started
             </button>
           </Link>
